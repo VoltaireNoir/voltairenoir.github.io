26cc336319c50470cb6a4cfe4199d42bca98ba20
VoltaireNoir <mzahmed95@gmail.com>
2023-03-27 16:48:30 +0530

    fix unwanted hover color change behavior

--- collapse.js
+++ collapse.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
   $(".block-header").click(function(event) {
     event.stopPropagation();
     $content = $(this).siblings(".block-content");
-    $content.slideToggle(200);
-    $content.parent().toggleClass("hidden");
+    if ($content.parent().hasClass("hidden")) {
+      $content.slideToggle(200);
+      $content.parent().toggleClass("hidden");
+    } else {
+      $content.slideToggle(200, function() {
+        $content.parent().toggleClass("hidden");
+      });
+    }
   });
 });
