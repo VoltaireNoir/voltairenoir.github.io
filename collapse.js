7a390a92b9525e24f861200a3e239fc665bafe38
VoltaireNoir <mzahmed95@gmail.com>
2023-03-27 15:46:50 +0530

    Change color behavior on collapsing

--- collapse.js
+++ collapse.js
@@ -1,6 +1,8 @@
-  $(document).ready(function() {
-    $(".block-header").click(function(event) {
-      event.stopPropagation();
-      $(this).next(".block-content").slideToggle(200);
-    });
+$(document).ready(function() {
+  $(".block-header").click(function(event) {
+    event.stopPropagation();
+    $content = $(this).siblings(".block-content");
+    $content.slideToggle(200);
+    $content.parent().toggleClass("hidden");
   });
+});
