f9dafc8c32f7022257245b9637243c143fc140be
VoltaireNoir <mzahmed95@gmail.com>
2023-03-29 15:02:15 +0530

    allow only one main block to be expanded

--- collapse.js
+++ collapse.js
@@ -1,14 +1,46 @@
 $(document).ready(function() {
   $(".block-header").click(function(event) {
     event.stopPropagation();
-    $content = $(this).siblings(".block-content");
-    if ($content.parent().hasClass("hidden")) {
-      $content.slideToggle(200);
-      $content.parent().toggleClass("hidden");
+    if ($(this).hasClass("main-header")) {
+      toggle_main(this);
     } else {
-      $content.slideToggle(200, function() {
-        $content.parent().toggleClass("hidden");
-      });
+      toggle(this);
     }
   });
 });
+
+function toggle(header) {
+  $content = $(header).siblings(".block-content");
+  $parent = $content.parent();
+  if ($parent.hasClass("hidden")) {
+    $parent.removeClass("hidden");
+    $content.slideDown(200);
+  } else {
+    $content.slideUp(200, function() {
+      $parent.addClass("hidden");
+    });
+  }
+}
+
+function toggle_main(header) {
+  // if the main block is not hidden, close it and early return
+  let $header = $(header);
+  let $parent = $header.parent();
+  if (!$parent.hasClass('hidden')) {
+    $header.siblings(".block-content").slideUp(200, function() {
+      $parent.addClass("hidden");
+    });
+    return;
+  }
+
+  $('.main').each(function() {
+    // if the main block is not hidden
+    if (!$(this).hasClass('hidden')) {
+      $(this).children(".block-content").slideUp(200);
+      $(this).addClass("hidden");
+    }
+  });
+
+  $header.siblings(".block-content").slideDown(200);
+  $parent.removeClass("hidden");
+}
